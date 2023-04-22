4c7e31791482a2e436ced327144431b5e1d87811
moayyadmn <moayyadvvv@gmial.com>
2023-04-22 18:51:57 +0300

    finish uploading json data to backend

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_quiz_app/firebse_ref/loading_status.dart';
 import 'package:firebase_quiz_app/firebse_ref/references.dart';
 import 'package:firebase_quiz_app/models/question_paper_model.dart';
 import 'package:flutter/services.dart';
@@ -14,7 +15,9 @@ class DataUploader extends GetxController {
     super.onReady();
   }
 
+  final loadingStatus = LoadingStatus.loading.obs;
   uploadData() async {
+    loadingStatus.value = LoadingStatus.loading;
     final firestore = FirebaseFirestore.instance;
     final String manifestContent = await DefaultAssetBundle.of(Get.context!)
         .loadString("AssetManifest.json");
@@ -29,8 +32,8 @@ class DataUploader extends GetxController {
     List<QuestionPaperModel> questionPaperList = <QuestionPaperModel>[];
     for (var paper in papersInAssets) {
       String stringPaperContent = await rootBundle.loadString(paper);
-      questionPaperList
-          .add(QuestionPaperModel.fromJson(jsonDecode(stringPaperContent)));// or we can do json.decode(stringPaperContent)
+      questionPaperList.add(QuestionPaperModel.fromJson(jsonDecode(
+          stringPaperContent))); // or we can do json.decode(stringPaperContent)
     }
     WriteBatch batch = firestore.batch();
     for (var paper in questionPaperList) {
@@ -41,7 +44,20 @@ class DataUploader extends GetxController {
         "questions_count":
             paper.questions == null ? 0 : paper.questions!.length,
       });
+      for (var question in paper.questions!) {
+        final questionPath =
+            questionRF(paperId: paper.id, questionId: question.id);
+        batch.set(questionPath, {
+          "quesion": question.quesion,
+          "correct_answer": question.correctAnswer,
+        });
+        for (var answer in question.answers) {
+          batch.set(questionPath.collection("answers").doc(answer.identifire),
+              {"identifire": answer.identifire, "Answer": answer.answer});
+        }
+      }
     }
     await batch.commit();
+    loadingStatus.value = LoadingStatus.completed;
   }
 }
--- lib/controllers/question_paper/data_uploader_screen.dart
+++ lib/controllers/question_paper/data_uploader_screen.dart
@@ -2,13 +2,19 @@ import 'package:firebase_quiz_app/controllers/question_paper/data_uploader.dart'
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../firebse_ref/loading_status.dart';
+
 class DataUploaderScreen extends StatelessWidget {
   DataUploaderScreen({super.key});
   final DataUploader controller = Get.put(DataUploader());
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('data uploadig...')),
+    return Scaffold(
+      body: Center(
+          child: Obx(() => Text(
+              controller.loadingStatus.value == LoadingStatus.completed
+                  ? 'Uploading completed 😍😍'
+                  : 'Uploadig...'))),
     );
   }
 }
